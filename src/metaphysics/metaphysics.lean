44bdeddd69622ec7cc3b2d213e547e240d1c8d30
Luiz Carlos Rumbelsperger Viana <luizcarlosrvn424@gmail.com>
2020-12-22 08:59:33 -0300

    Partially modularized metaphysics.lean

--- src/metaphysics/metaphysics.lean
+++ src/metaphysics/metaphysics.lean
@@ -1,64 +1,17 @@
-import metaphysics.causality metaphysics.finitism
+import metaphysics.causality metaphysics.finitism states
 universe u
 open set topological_space classical
 local attribute [instance] prop_decidable
 
 namespace ontology
-
     
-    section metaphysics
-    
-        variables {ω : ontology} (s : ω.substance)
+  variables {ω : ontology} (s : ω.substance)
 
-        -- We call a substance `metaphysical`, or a `separate substance`, if it is simple.
-        -- It is otherwise `physical`, or a `body`, as already mentioned.
-        @[reducible]
-        def substance.metaphysical := simple s.val
-        def separate (ω : ontology) := subtype {s : ω.substance | s.metaphysical}
-        
-        -- A substance is purely actual if it has no passive potency
-        -- to be different from what it is, i.e. if it has a single state.
-        -- def substance.purely_actual := nonempty (s.state ≃ unit)
-        
-        -- Only the necessary being can be purely actual, 
-        -- in which case platonism follows.
-        -- def eq_nb_of_purely_actual : ∀ s, purely_actual s → s = nb :=
-        -- begin
-        --     intros s h,
-        --     simp [purely_actual] at h,
-        --     apply nonempty.elim h,
-        --     intro iso,
-        --     simp [substance.state, substance.state_setoid, quotient] at iso,
-            
-        -- end
-
-        -- theorem metaphysical_of_purely_actual : s.purely_actual → s.metaphysical  := sorry
-        
-        -- Platonism in the broad sense is the doctrine 
-        -- that there are metaphysical substances
-        def platonism (ω : ontology) := nonempty ω.separate
-
-        -- theism is the doctrine that the necessary being is metaphysical.
-        def theism (ω : ontology) := ω.nb.metaphysical
-        
-        -- Platonism is logically equivalent to theism
-        lemma platonism_of_nonempty_separate :  ω.platonism ↔ ω.theism := sorry
-        
-    end metaphysics
+  -- (strong) Platonism is the doctrine 
+  -- that there are simple substances
+  def splatonism (ω : ontology) := ∃ s : ω.substance, s.simple
 
     section theology
-        
-        variable (ω : ontology)
-
-        -- Classical Theism is an extension of theism which 
-        -- furthermore claims that there is a possible world 
-        -- in which the necessary being exists alone.
-        def ctheism := ∃ (w : ω.world), world.entities w = {ω.nb.val}
-
-        theorem ctheism_stronger : ω.ctheism → ω.theism := sorry
-
-        -- greek theism is non-classical theism
-        def greek_theism := ω.theism ∧ ¬ ∃ (w : ω.world), world.entities w = {ω.nb.val}
         
         -- The leibnizian version of a weakened version of Aquina's second way.
         -- Possibly the weakest argument anyone can give for the existence of God.
@@ -66,7 +19,7 @@ namespace ontology
             begin
                 simp [set.nonempty, epsr],
                 intros w psr,
-                dunfold theism substance.metaphysical simple,
+                dunfold theism substance.simple simple,
                 -- suppose God had an accident
                 by_contradiction h,
                 push_neg at h,
---
+++ src/theology/natural/god.lean
@@ -0,0 +1,61 @@
+import metaphysics.metaphysics
+open set topological_space classical
+local attribute [instance] prop_decidable
+
+namespace ontology
+
+
+section theism
+  
+  variable (ω : ontology)
+
+  -- theism is the doctrine that the necessary being is simple.
+  def theism (ω : ontology) := ω.nb.simple
+        
+  -- strong Platonism is logically equivalent to theism
+  lemma splatonism_iff_theism :  ω.splatonism ↔ ω.theism := sorry
+
+
+  -- Classical Theism is an extension of theism which 
+  -- furthermore claims that there is a possible world 
+  -- in which the necessary being exists alone.
+  def ctheism := ∃ (w : ω.world), w.entities = {ω.nb.val}
+
+  -- this is a not so trivial proof that if the nb exists alone,
+  -- then it has no accidents, because accidents are contingent.
+  @[simp] theorem theism_of_ctheism : ω.ctheism → ω.theism := sorry
+
+  -- greek theism is non-classical theism
+  def greek_theism := ω.theism ∧ ¬ ω.ctheism
+
+end theism
+
+section divine_substances
+
+  variables {ω : ontology} (s : ω.substance)
+
+  -- A substance is purely actual if it has no passive potency
+  -- to be different from what it is, i.e. if it has a single state.
+  def substance.purely_actual := ∀ w₁ w₂ : ω.world, s.state w₁ = s.state w₂
+  
+  -- Only the necessary being can be purely actual, 
+  -- in which case theism follows.
+  def eq_nb_of_purely_actual : s.purely_actual → s = ω.nb :=
+  begin
+      intros h,
+      simp [substance.purely_actual] at h,
+      ext,
+      simp [nb, nbe],
+      obtain ⟨y, hy⟩ := s.val.ne,
+      specialize h x y,
+      replace hy := in_state_of_exists.mp hy,
+      rw ←h at hy,
+      exact hy,
+  end
+
+  
+  theorem simple_of_purely_actual : s.purely_actual → s.simple  := sorry
+
+end divine_substances
+
+end ontology
