0796daa1612b551e89301987e83e38319db65f9a
Luiz Carlos Rumbelsperger Viana <luizcarlosrvn424@gmail.com>
2021-01-19 09:13:04 -0300

    Added definitions for hylemorphism and the 'universe', changed docstrings

--- src/metaphysics/metaphysics.lean
+++ src/metaphysics/metaphysics.lean
@@ -7,9 +7,17 @@ namespace ontology
     
   variables {ω : ontology} (s : ω.substance)
 
-  -- (strong) Platonism is the doctrine 
-  -- that there are simple substances
+  /-- (strong) Platonism is the doctrine 
+      that there are simple substances. -/
   def splatonism (ω : ontology) := ∃ s : ω.substance, s.simple
+
+  def suniversal_hylemorphism := ¬∃ s : ω.substance, s.simple
+  def universal_hylemorphism := ¬∃ s : ω.substance, s.contingent ∧ s.simple
+
+  /- The (contingent) universe is the supremum of all contingent
+      things. It is the fact that there are contingent entities. -/
+  -- def «universe» (ω : ontology) : ω.substance :=  
+  --   Sup {e : ω.substance | e.contingent} --h, sorry⟩
 
 
 end ontology
