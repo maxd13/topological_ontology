dca609d5fc94ed1a4e907d3eac7145b2dc73ede8
Luiz Carlos Rumbelsperger Viana <luizcarlosrvn424@gmail.com>
2021-10-20 09:21:30 -0300

    Added construction of alexandroff space from partial order

--- src/math/alexandroff_space.lean
+++ src/math/alexandroff_space.lean
@@ -20,14 +20,13 @@ class alexandroff_space (α : Type u) [t : topological_space α] : Prop :=
   (alexandroff : ∀ {I} (f : I → set α), (∀ i, t.is_open (f i)) → t.is_open ⋂ i, f i)
 
 variables {α : Type u} (t : topological_space α)
-variables (x y : α) 
 include t
 
-def specialization : Prop :=
+def specialization (x y : α) : Prop :=
   ∀ s, t.is_open s → x ∈ s → y ∈ s 
 
-
-instance specialization_order [h : t0_space α] : partial_order α := 
+@[priority std.priority.default - 100]
+instance specialization_order [h : @t0_space α t] : partial_order α := 
 { le := t.specialization,
   le_refl := by simp [specialization],
   le_trans := by finish [specialization],
@@ -58,7 +57,66 @@ instance sierpinski_t0 : @t0_space Prop sierpinski_space :=
     contradiction,
   end
 
+def upper₀ [partial_order α] (s : set α) : Prop := 
+  ∀ x y : α, x ≤ y → x ∈ s → y ∈ s
 
+def alexandroff₀_of : partial_order α → topological_space α :=
+  assume order, 
+  { is_open := @upper₀ α order,
+  is_open_univ := by simp [upper₀],
+  is_open_inter := 
+  begin
+    intros s₁ s₂ h₁ h₂,
+    simp [upper₀] at *,
+    intros x y h₃ h₄ h₅, 
+    constructor,
+      apply h₁ x y; assumption,
+    apply h₂ x y; assumption,
+  end,
+  is_open_sUnion :=
+  begin
+    intros S h,
+    simp [upper₀],
+    intros x y h₁ s h₂ h₃,
+    refine ⟨s, h₂, _⟩,
+    specialize h s h₂, simp [upper₀] at h,
+    apply h; assumption,
+  end }
+
+instance alexandroff₀_t0  (order) : @t0_space α (alexandroff₀_of order) :=
+  let le := @partial_order.le _ order,
+      upper := @upper₀ _ order
+  in
+  begin
+    constructor,
+    intros x y h₀,
+    by_cases h : le x y,
+      use {w | le y w},
+      constructor,
+        change (upper {w : α | le y w}), 
+        simp [upper, upper₀],
+        intros a b h₁ h₂,
+        apply @partial_order.le_trans _ order y; 
+        assumption,
+      have c : le y y := @partial_order.le_refl _ order y,
+      simp [xor],
+      change (le y x ∧ ¬le y y ∨ le y y ∧ ¬le y x),
+      simp [c],
+      intros insanity,
+      have c := @le_antisymm _ order x y h insanity,
+      contradiction,
+    use {w | le x w},
+    constructor,
+      change (upper {w : α | le x w}),
+      simp [upper, upper₀],
+      intros a b h₁ h₂,
+      apply @partial_order.le_trans _ order x; 
+      assumption,
+    have c : le x x := @partial_order.le_refl _ order x,
+    simp [xor],
+    change (le x x ∧ ¬le x y ∨ le x y ∧ ¬le x x),
+    simpa [c],
+  end
 
 end topological_space
 
